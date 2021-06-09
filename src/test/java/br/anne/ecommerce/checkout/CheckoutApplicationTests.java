dd6e753112b79d68a8222a0bc05c38308cd16f42
annezdz <annezdz@hotmail.com>
2021-06-09 13:05:46 -0300

    initial commit

---
+++ src/test/java/br/anne/ecommerce/checkout/CheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package br.anne.ecommerce.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
